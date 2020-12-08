6c6d9d0d72c332c08e8bec9c371f23dc4465f82f
pratyay360 <pratyaymustafi@gmail.com>
2020-12-08 16:40:04 +0530

    UPDATED STATIC

--- epair/settings.py
+++ epair/settings.py
@@ -123,11 +123,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 #added static files here
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR,"static"),
     '/var/www/static/',
 ]
+
 AUTH_USER_MODEL = 'login.CustomUser'
 
 # Fast2sms API Configuration
